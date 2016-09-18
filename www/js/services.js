20c2491348cf75568500a3a257f48ea8a498f73c
lejayson <jayson_lee@live.com>
2016-09-18 01:12:51 -1000

    Responder app (#10)
    * jayson services edit
    
    added test fields
    
    * Jayson edit
    
    Filter added to responder
    
    * Jayson Edit
    
    Fix responder markers
    
    * jayson edit
    
    ionic uplink
    
    * responder app pre-screening submit
    
    Jayson Edit

--- www/js/services.js
+++ www/js/services.js
@@ -10,13 +10,18 @@ angular.module('app.services', [])
         setLoc: function(latlng) {
             Location = latlng;
         }
-    }
+    };
 })
 
 .service('$infoProperties', function() {
     var name;
 	var gender;
 	var description;
+	var environment;
+	var isGroup;
+	var ageGroup;
+  var subemail;
+  var subphone;
     
     return {
         getName: function() {
@@ -36,7 +41,50 @@ angular.module('app.services', [])
         },
         setDesc: function(desc) {
             description = desc;
+        },
+		getEnv: function() {
+            return environment;
+        },
+        setEnv: function(env) {
+            environment = env;
+        },
+		getAdult: function() {
+            return Adult;
+        },
+        setAdult: function(adl) {
+            Adult = adl;
+        },
+        getChild: function() {
+            return Children;
+        },
+        setChild: function(chd) {
+            Children = chd;
+        },
+		getisGroup: function() {
+            return isGroup;
+        },
+        setisGroup: function(grp) {
+            isGroup = grp;
+        },
+		getInv: function() {
+            return ageGroup;
+        },
+        setInv: function(inv) {
+            ageGroup = inv;
+		},
+        getEmail: function() {
+            return subemail;
+        },
+        setEmail: function(eml) {
+            subemail = eml;
+        },
+        getPhone: function() {
+            return subphone;
+        },
+        setPhone: function(phn) {
+            subphone = phn;
         }
+        
     }
 });
 
